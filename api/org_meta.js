9c9a40f89ea8c9c89ffde3fec11f6a4092392560
Baza-812 <v.sp77@tutanota.com>
2025-11-28 12:37:18 +0300

    Create org_meta.js

---
+++ api/org_meta.js
@@ -0,0 +1,18 @@
+{
+  "ok": true,
+  "vidDogovora": "Starshiy",
+  "priceFull": 350,
+  "priceLight": 290,
+  "minTeamSize": 8,
+  "freeDeliveryMinOrders": 8,
+  "bank": {
+    "name": "ИП Иванов · Тинькофф",
+    "legalName": "ИП Иванов Иван Иванович",
+    "inn": "123456789012",
+    "account": "40817...",
+    "bic": "044525974",
+    "contactPhone": "+7...",
+    "contactEmail": "support@...",
+    "footerText": "Реквизиты для оплаты: ..."
+  }
+}
